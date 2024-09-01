247e4c0459f063c2f3ab2954dc435b024215c041
EC2 Default User <ec2-user@ip-172-31-21-0.ec2.internal>
2024-09-01 22:27:44 +0000

    push main and output TF

---
+++ jump-server/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket         = "5907-terraform-epam-cora"
+    key            = "cora1-jumpbox/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "epam-cora-terraform"
+  }
+  required_version = "1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module jump-server {
+  source                            = "git::https://github.com/epam/cloud-pipeline//deploy/infra/aws/terraform/cloud-native/jump-server?ref=<branch-tag-or-commit>"
+  project_name                      = "epam-cora-terraform1"
+  env                               = "epam-cora-terraform-env1"
+  vpc_id                            = "vpc-05deec794c0d4e0c1"
+  subnet_id                         = "subnet-003d978a107ffa704"
+  iam_role_permissions_boundary_arn = "arn:aws:iam::590788709872:role/epam-cora-jump-server-role"
+}
---
+++ jump-server/output.tf
@@ -0,0 +1,11 @@
+output "instance_connection" {
+  value = module.jump-server.output_message
+}
+
+output "instance_id" {
+  value = module.jump-server.jump_sever_id
+}
+
+output "instance_role" {
+  value = module.jump-server.jump_server_role
+}
